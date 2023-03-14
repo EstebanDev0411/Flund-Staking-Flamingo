22fcc35cf72db70f45d697f6b317040a759fab8a
tomio111 <tobilokki99@gmail.com>
2023-03-14 11:06:29 -0700

    initial commit

---
+++ Flund-Staking-Flamingo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Attributes;
+
+namespace Flund_Staking_Flamingo
+{
+    [DisplayName("Flund_Staking_Flamingo")]
+    [ManifestExtra("Author", "NEO")]
+    [ManifestExtra("Email", "developer@neo.org")]
+    [ManifestExtra("Description", "This is a Flund_Staking_Flamingo")]
+    public class Flund_Staking_Flamingo : SmartContract
+    {
+        public static bool Main()
+        {
+            return true;
+        }
+    }
+}
